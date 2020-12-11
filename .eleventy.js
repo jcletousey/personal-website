fb5ddb46f5e2e285c80c92a99a787458a567a7ac
Jean-Charles LE TOUSEY <jean-charles.letousey@fime.com>
2020-12-11 17:35:06 +0100

    Update npm scripts

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addWatchTarget("./src/sass/");
+
   return {
     dir: {
       input: "src",
