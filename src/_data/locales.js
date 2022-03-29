d1a374b07b76cb33cea480b8c65b2ce35f58447d
Jean-Charles LE TOUSEY <jcltousey@gmail.com>
2022-03-29 22:23:15 +0200

    Change path from available locales

--- src/_data/locales.js
+++ src/_data/locales.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 module.exports = function () {
   const declaredLocales = Object.keys(require("./site.json").locales);
-  const availableLocales = declaredLocales.filter(locale => fs.existsSync(`./src/${locale}`));
+  const availableLocales = declaredLocales.filter(locale => fs.existsSync(`./src/content/${locale}`));
 
   return availableLocales;
 }
